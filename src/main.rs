9d2e4382080be60baf820de2de556889dfcc112f
bstaint <bstaint@gmail.com>
2021-03-29 21:09:00 +0800

    Add label_button.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use crate::custom_widget::CustomWidget;
 use crate::delegate::Delegate;
-use druid::widget::{Button, Flex};
+use druid::widget::{Flex, Label, Painter};
 use druid::{
-    commands, AppLauncher, Command, FileDialogOptions, FileSpec, LocalizedString, Target, Widget,
-    WidgetExt, WindowDesc,
+    commands, AppLauncher, Color, Command, Env, EventCtx, FileDialogOptions, FileSpec,
+    LocalizedString, RenderContext, Target, Widget, WidgetExt, WindowDesc,
 };
 use druid_image_viewer::*;
+
+fn label_button(
+    text: &'static str,
+    f: impl Fn(&mut EventCtx, &mut AppState, &Env) + 'static,
+) -> impl Widget<AppState> {
+    let painter = Painter::new(|ctx, _, _env| {
+        let bounds = ctx.size().to_rect();
+        ctx.fill(bounds, &Color::rgb8(155, 183, 214));
+        if ctx.is_active() {
+            ctx.fill(bounds, &Color::rgb8(0x71, 0x71, 0x71));
+        }
+    });
+
+    Label::new(LocalizedString::new(text))
+        .padding(2.)
+        .background(painter)
+        .on_click(f)
+}
 
 fn ui_builder() -> impl Widget<AppState> {
     let open_dialog_options = FileDialogOptions::new()
@@ -17,26 +35,18 @@ fn ui_builder() -> impl Widget<AppState> {
     Flex::column()
         .with_child(
             Flex::row()
-                .with_child(
-                    Button::new(LocalizedString::new("open-button"))
-                        .padding(5.)
-                        .on_click(move |ctx, _, _| {
-                            ctx.submit_command(Command::new(
-                                commands::SHOW_OPEN_PANEL,
-                                open_dialog_options.clone(),
-                                Target::Auto,
-                            ));
-                        }),
-                )
+                .with_child(label_button("open-button", move |ctx, _, _| {
+                    ctx.submit_command(Command::new(
+                        commands::SHOW_OPEN_PANEL,
+                        open_dialog_options.clone(),
+                        Target::Auto,
+                    ));
+                }))
                 .with_flex_spacer(1.0)
-                .with_child(
-                    Button::new(LocalizedString::new("grey-button"))
-                        .padding(5.)
-                        .on_click(move |ctx, _, _| {
-                            ctx.submit_command(Command::new(SET_IMAGE_GRAY, (), Target::Auto))
-                        }),
-                )
-                .fix_height(35.),
+                .with_child(label_button("grey-button", |ctx, _, _| {
+                    ctx.submit_command(Command::new(SET_IMAGE_GRAY, (), Target::Auto))
+                }))
+                .fix_height(25.),
         )
         .with_child(CustomWidget {})
 }
@@ -48,7 +58,6 @@ fn main() {
         .title("Image Viewer");
 
     AppLauncher::with_window(window)
-        .log_to_console()
         .delegate(Delegate)
         .launch(AppState::default())
         .expect("Failed to launch application")
